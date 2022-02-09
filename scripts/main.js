a9fd943627e07c1e1ddad303a6f147cb6743a1a7
Paul Cheung <44212283+pineapppaul@users.noreply.github.com>
2022-02-09 13:54:27 -0500

    Major changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+window.addEventListener('load', displayMenu);
 // function createCard(){ 
 //     $("main").append(`<div class="card"></div>`)
 // }
@@ -51,14 +51,15 @@ function generateRandomImageURL(){
   return `${roboHash}${roboSet}${randomImgId}${roboSize}`
 }
 
+let difficulty;
 let rows;
 let columns;
 
 
 
 function promptTable() {
-  let difficulty = prompt("Enter: 'Easy','Medium','Hard'");
-  switch(difficulty.toLocaleLowerCase()) {
+  
+  switch(difficulty) {
     case "easy":
       console.log("easy")
       rows = 4;
@@ -88,10 +89,9 @@ function promptTable() {
 }
 
 function showTable() {
-  let myBody = document.getElementsByTagName("body")[0];
+  let myBody = document.getElementsByTagName("main")[0];
   myBody.innerHTML = " ";
   myBody.style.background = "#C1B283";
-  myBody.style.marginTop = "100px";
   myBody.style.marginLeft = "100px";
   let container = document.createElement("div");
 
@@ -174,6 +174,32 @@ function showTable() {
  
 }
 
-promptTable();
+
+
+
+
+// promptTable();
+
+function displayMenu() {
+  $(".container").html('<div class="contentBox"> <h2><br>---------- </h2> <h3>Difficulty</h3> <form class="centerText" action="/main.js"> <select name="difficulty" id="difficulty" class="formMargin"> <option value="easy">Easy</option> <option value="medium">Medium</option> <option value="hard">Hard</option> <option value="haveFunGuessing">Have Fun Guessing</option> </select> <br><h3>Game Size</h3> <select name="size" id="size" class="formMargin"> <option value="small">Small 4x4</option> <option value="medium">Medium 6x6</option> <option value="large">Large 8x8</option> </select> <br><h3>Players</h3> <select name="players" id="players" class="formMargin"> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> </select> <br><h3>Card Images</h3> <select name="imageSet" id="imageSet" class="formMargin"> <option value="1">Mixed Bag</option> <option value="2">Robots</option> <option value="3">Robo Heads</option> <option value="4">KITTIES!</option> </select> <br><input name="playButton" type="button" id="playButton" value="Play!" class="btn btn-success playBtn mt-4"> </form> <div class="centerText"> <button type="button" id="btnShowInstructions" class="btn btn-info">How to Play</button> </div><div id="instructions"> <div class="col-6 offset-3 pt-5"> <h2 class="text-center">How to Play</h2> <h3 class="pt-3">To Win the Game</h3> <p class="pt-2">This is a dangerously-fun game to test your memory-- you have limited attempts to match all the cards face.</p><h3 class="pt-3">Levels of Difficulty</h3> <ul class="pt-2"> <li>Easy - Find 8 pairs</li><li>Medium - Find 10 pairs</li><li>Hard - Find 11 pairs</li></ul> <div class="text-center pt-3"> <button id="btnCloseInstructions" class="btn btn-primary">Back to Menu</button> </div></div></div>')
+
+  $("#btnShowInstructions").click(showInstructions);
+  $("#btnCloseInstructions").click(closeInstructions);
+  $("#playButton").click(function(){
+    difficulty = $("#difficulty").val();
+    promptTable();
+  });
+}
+
+function showInstructions() {
+  $("#instructions").css("display", "block");
+  console.log("asda")
+}
+
+function closeInstructions() {
+  $("#instructions").hide();
+}
+
+// event listeners
 
 
